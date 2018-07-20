1a6910b7cc73c6957d7a37c73c69d555803003a4
James Chambers <jameschambers2@gmail.com>
2018-07-20 03:51:23 -0400

    better OSReport def

--- hello_osreport_c/source/template.c
+++ hello_osreport_c/source/template.c
@@ -1,13 +1,8 @@
 #define OS_REPORT 0x8005A750
 
-void (*__OSReport)(char*) = (void*) OS_REPORT;
-
-void OSReport(char *message) {
-  __OSReport(message);
-  return;
-}
+void (*OSReport)(char*, ...) = (void*) OS_REPORT;
 
 void __entry() {
-  OSReport("hello world!");
+  OSReport("hello world! the number of the day is %u", 1337);
   return;
 }
