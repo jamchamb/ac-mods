00d6e4922240f2ef2ebb83317c9d5eca3bb75ab4
James Chambers <jameschambers2@gmail.com>
2018-07-21 16:28:19 -0400

    optimize branching to AC function

---
+++ hello_osreport_c/source/aclib.h
@@ -0,0 +1,6 @@
+#ifndef ACLIB_H
+#define ACLIB_H
+
+extern void OSReport(char*, ...);
+
+#endif
--- hello_osreport_c/source/template.c
+++ hello_osreport_c/source/template.c
@@ -1,6 +1,4 @@
-#define OS_REPORT 0x8005A750
-
-void (*OSReport)(char*, ...) = (void*) OS_REPORT;
+#include "aclib.h"
 
 void __entry() {
   OSReport("hello world! the number of the day is %u", 1337);
